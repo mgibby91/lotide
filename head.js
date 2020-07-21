237d07b7281720a07b45d8fe9f9c3611f09b5a9e
Matthew Gibson <mattgibson6@gmail.com>
2020-07-20 19:11:55 -0600

    head function created

---
+++ head.js
@@ -0,0 +1,19 @@
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🤑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤮 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const head = function (array) {
+
+  return array[0];
+
+}
+
+assertEqual(head([]), 5);
+assertEqual(head(['hello', 'lighthouse', 'labs']), 'hello');
