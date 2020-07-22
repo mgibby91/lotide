97949a406e54266ab024227753ce11430d33556b
Matthew Gibson <mattgibson6@gmail.com>
2020-07-21 19:54:57 -0600

    middle.js assignment complete

---
+++ middle.js
@@ -0,0 +1,45 @@
+
+const eqArrays = function (array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+
+}
+
+const assertArraysEqual = function (array1, array2) {
+
+  const areArraysEqual = eqArrays(array1, array2);
+
+  if (areArraysEqual) {
+    console.log(`🤑 Assertion Passed: ${array1} equals ${array2}`);
+  } else {
+    console.log(`🤮 Assertion Failed: ${array1} does not equal ${array2}`);
+  }
+
+}
+
+const middle = function (array) {
+
+  if (array.length <= 2) return [];
+
+  if (array.length % 2 !== 0) return [array[array.length / 2 - 0.5]];
+
+  if (array.length % 2 === 0) return [array[array.length / 2 - 1], array[array.length / 2]];
+
+}
+
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
