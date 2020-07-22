33d5bddc5b4d48e6d265a81c6e0bff986c66fd95
Matthew Gibson <mattgibson6@gmail.com>
2020-07-21 21:28:24 -0600

    countLetters.js assignment completed

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+const assertEqual = function (actual, expected) {
+
+  if (actual === expected) {
+    console.log(`🤑 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤮 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+
+const countLetters = function (string) {
+
+  let answer = {};
+
+  for (let char of string) {
+    if (char !== ' ') {
+      if (answer[char]) {
+        answer[char] += 1;
+      } else {
+        answer[char] = 1;
+      }
+    }
+  }
+
+  return answer;
+
+}
+
+console.log(countLetters('lighthouse in the house'));
