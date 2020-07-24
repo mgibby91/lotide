b92a3024d8c0304a4f8e21791a4b695c222b841a
Matthew Gibson <mattgibson6@gmail.com>
2020-07-23 20:04:27 -0600

    eqArrays.js updated to include nested arrays in arrays

--- eqArrays.js
+++ eqArrays.js
@@ -16,7 +16,9 @@ const eqArrays = function (array1, array2) {
   }
 
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+    if (Array.isArray(array1[i]) && Array.isArray(array2[i]) && eqArrays(array1[i], array2[i])) {
+      continue;
+    } else if (array1[i] !== array2[i]) {
       return false;
     }
   }
@@ -25,8 +27,14 @@ const eqArrays = function (array1, array2) {
 
 }
 
+
+
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
-assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), true);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+assertEqual(eqArrays([[2, 3[4, 4]], [5, 4]], [[2, 3[4, 4]], [5, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => false
 
