13ed276fd0206d1fe556ce5568b2a2df7b205bb7
Matthew Gibson <mattgibson6@gmail.com>
2020-07-27 10:20:03 -0600

    assertArraysEqual file name changed, middle refactored for modules and test file created in /test

--- middle.js
+++ middle.js
@@ -1,33 +1,33 @@
 
-const eqArrays = function (array1, array2) {
+// const eqArrays = function (array1, array2) {
 
-  if (array1.length !== array2.length) {
-    return false;
-  }
+//   if (array1.length !== array2.length) {
+//     return false;
+//   }
 
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
+//   for (let i = 0; i < array1.length; i++) {
+//     if (array1[i] !== array2[i]) {
+//       return false;
+//     }
+//   }
 
-  return true;
+//   return true;
 
-}
+// }
 
-const assertArraysEqual = function (array1, array2) {
+// const assertArraysEqual = function (array1, array2) {
 
-  const areArraysEqual = eqArrays(array1, array2);
+//   const areArraysEqual = eqArrays(array1, array2);
 
-  if (areArraysEqual) {
-    console.log(`🤑 Assertion Passed: ${array1} equals ${array2}`);
-  } else {
-    console.log(`🤮 Assertion Failed: ${array1} does not equal ${array2}`);
-  }
+//   if (areArraysEqual) {
+//     console.log(`🤑 Assertion Passed: ${array1} equals ${array2}`);
+//   } else {
+//     console.log(`🤮 Assertion Failed: ${array1} does not equal ${array2}`);
+//   }
 
-}
+// }
 
-const middle = function (array) {
+const middle = function(array) {
 
   if (array.length <= 2) return [];
 
@@ -37,9 +37,5 @@ const middle = function (array) {
 
 }
 
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
+
