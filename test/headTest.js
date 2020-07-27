66a113778c0af3da098e04050e7f3bf2b01325f0
Matthew Gibson <mattgibson6@gmail.com>
2020-07-27 13:21:47 -0600

    headTest.js file includes chai/mocha testing now

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,16 @@
 
 const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-assertEqual(head([]), 5);
-assertEqual(head(['hello', 'lighthouse', 'labs']), 'hello');
+
+describe('#head', () => {
+  it('returns 1 for array [1,2,3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it('returns "5" for ["5"]', () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+  it('returns undefined for []', () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
